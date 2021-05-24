00b06952cb1135268b83650602173e879ab144b3
Robson Souza Lourenço <robinhosl2008@gmail.com>
2021-05-24 19:57:06 -0300

    Reenviando branch para refazer o PR.

---
+++ src/models/profession.ts
@@ -0,0 +1,18 @@
+import {
+	IsString,
+	Length
+} from 'class-validator';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { BaseEntity } from './base_entity';
+import { Specialists } from './specialists';
+
+@Entity()
+export class Profession extends BaseEntity<Profession> {
+	@IsString({ always: true })
+	@Length(3, 100, { always: true })
+	@Column({ type: 'varchar', nullable: false })
+	name: string;
+
+	@OneToMany(() => Specialists, specialists => specialists.profession)
+	specialists: Specialists[];
+}
