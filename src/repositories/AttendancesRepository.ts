f88896ce75d9858fe94d5e88a5d110b9b07707fe
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-05-27 18:41:08 -0300

    Revert "Criando a rotina de Atendimentos"

--- src/repositories/AttendancesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Attendance } from './../models/attendance';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(Attendance)
-class AttendanceRepository extends Repository<Attendance> {}
-
-export { AttendanceRepository };
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import { attendanceRouter } from './routes/AttendanceRouter';
 
 const router = Router();
 
 router.get('/', (request, response) => {
   return response.json({ message: 'Afya Challenge - Team 1' });
 });
-
-router.use('./attendances', attendanceRouter);
 
 export { router };
