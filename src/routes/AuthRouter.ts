b8e1ab222906edd2a05d3e18b9e4989c013e58dd
fellipehfa <fellipehf@gmail.com>
2021-06-08 19:02:30 -0300

    try prod connection

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
-import { AuthController } from '../controllers/AuthController';
-import authMiddleware from '../middlewares/AuthMiddleware';
+// import { Router } from 'express';
+// import { AuthController } from '../controllers/AuthController';
+// import authMiddleware from '../middlewares/AuthMiddleware';
 
-const AuthRouter = Router();
-const authController = new AuthController();
+// const AuthRouter = Router();
+// const authController = new AuthController();
 
-AuthRouter.post('/login', authController.authenticate);
+// AuthRouter.post('/login', authController.authenticate);
 
-export { AuthRouter };
+// export { AuthRouter };
