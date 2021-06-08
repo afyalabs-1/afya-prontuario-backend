b3a6d9054e60edd5e53be9a33cf95f4be4155605
fellipehfa <fellipehf@gmail.com>
2021-06-08 18:29:23 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import { router } from './routes';
 import { AppError } from './error/AppError';
 
 const app = express();
 
+// app.use(cors({
+//   origin: ['URL PROD']
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
