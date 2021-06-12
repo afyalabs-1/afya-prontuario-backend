fe95a642ecad9c5278b3488ccca3fe32d44ccf25
robson.lourenco <robson.lourenco@pebmed.com.br>
2021-06-12 00:12:56 -0300

    Atualizando nome do diretório de testes e removendo arquivos de teste de exemplo

--- src/test/responseError.test.ts
+++
@@ -1,8 +0,0 @@
-import { soma } from './teste';
-
-describe('Teste para saber se o Jest está funcional.', () => {
-    test('Soma entre dois números decimais', () => {
-        expect(soma(1, 2)).toBe(3);
-    }) ;
-});
-
