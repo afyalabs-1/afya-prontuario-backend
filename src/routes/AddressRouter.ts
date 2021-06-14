07a9b6e70dddd5390882b1a1404b911b38c4af56
fellipehfa <fellipehf@gmail.com>
2021-06-14 14:11:08 -0300

    request auth for address route

--- src/routes/AddressRouter.ts
+++ src/routes/AddressRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { AddressController } from '../controllers/AddressController';
+import authMiddleware from '../middlewares/AuthMiddleware';
 
 const AddressRouter = Router();
 const addressController = new AddressController();
+
+AddressRouter.use(authMiddleware);
 
 AddressRouter.post('/', addressController.create);
 AddressRouter.get('/', addressController.list);
